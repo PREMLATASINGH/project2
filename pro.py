3949bdf9c48ba26f602f62d11df3ae8ec63e05f2
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-07-23 15:07:07 -0400

    sort

--- pro.py
+++ pro.py
@@ -42,3 +42,4 @@ print(df[(df['purchase_amount'] < 200) & (df['product_category'] == 'Clothing')]
 print(df[(df['purchase_amount'] < 200) & (df['product_category'] == 'Clothing')].shape[0])
 print(df.groupby('product_category')['purchase_amount'].agg(['mean', 'sum', 'count']))
 print(df.groupby('product_category')['purchase_amount'].agg(['mean', 'sum', 'count']).reset_index())
+print(df.groupby('product_category')['purchase_amount'].agg(['mean', 'sum', 'count']).reset_index().sort_values(by='sum', ascending=False))
